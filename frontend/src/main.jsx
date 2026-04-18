2bbd22431353a0f0094e194870857add97e01011
Abdullah Bakit <abdullahbakit2006@gmail.com>
2026-04-18 22:00:02 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import UserApp from './userApp';
+import AdminApp from './adminApp';
+import ResearcherApp from './researcherApp';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
